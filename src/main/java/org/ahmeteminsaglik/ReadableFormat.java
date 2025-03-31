af8a94242d8c31d901f2be30e13b29f1ff2b7f79
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2025-03-31 19:17:01 +0300

    - primitive types are convert to Wrapper types. - version is updated to 1.2

--- src/main/java/org/ahmeteminsaglik/ReadableFormat.java
+++ src/main/java/org/ahmeteminsaglik/ReadableFormat.java
@@ -2,22 +2,26 @@ package org.ahmeteminsaglik;
 
 public class ReadableFormat {
 
-    public static String getStringValue(int num) {
+    public static String getStringValue(Integer num) {
+        if (num == null) return null;
         String numberText = Utility.convertIntegerToString(num);
         return Utility.getProcessedNumberText(numberText);
     }
 
-    public static String getStringValue(long num) {
+    public static String getStringValue(Long num) {
+        if (num == null) return null;
         String numberText = Utility.convertLongToString(num);
         return Utility.getProcessedNumberText(numberText);
     }
 
-    public static String getStringValue(double num) {
+    public static String getStringValue(Double num) {
+        if (num == null) return null;
         String numText = Utility.convertDoubleToString(num);
         return Utility.getProcessedNumbersHavingDecimal(numText);
     }
 
-    public static String getStringValue(float num) {
+    public static String getStringValue(Float num) {
+        if (num == null) return null;
         String numText = Utility.convertFloatToString(num);
         return Utility.getProcessedNumbersHavingDecimal(numText);
     }
